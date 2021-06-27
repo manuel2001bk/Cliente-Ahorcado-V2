b73fe5c81c5064c87633646ba9d3376a0d5dc0cb
manuel2001bk <manuel2001bk@gmail.com>
2021-06-27 16:34:35 -0500

    add verificación de palabra y visualización de palabra en el front

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import update from "immutability-helper";
 import css from '../assets/css/app.css'
 import imagen1 from '../assets/img/horcado1.png'
 import imagen2 from '../assets/img/horcado2.png'
@@ -6,38 +7,90 @@ import imagen3 from '../assets/img/horcado3.png'
 import imagen4 from '../assets/img/horcado4.png'
 import imagen5 from '../assets/img/horcado5.png'
 
+import { io } from "socket.io-client";
+
+
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            palabra : ""
+            palabra : "",
+            palabraConGuiones : '',
+            letra : ""
         }
+       this.socket = io("ws://localhost:3000")
+    }
+    changeField(e) {
+        let field = e.target.name
+        let value = e.target.value
+
+        this.setState(update(this.state, {
+            [field] : {$set : value}
+        }))
     }
 
-    ingresarletra(){
+    ingresarletra(e){
+        String.prototype.replaceAt = function(index, character) {
+            return this.substr(0, index) + character + this.substr(index+character.length);
+        }
+        for (const i in this.state.palabra){
+            if(this.state.letra == this.state.palabra[i]){
+                //alert("exito")
+                this.state.palabraConGuiones = this.state.palabraConGuiones.replaceAt(i*2, this.state.letra);
+                this.palabraGuiones.innerHTML = this.state.palabraConGuiones
+            }
+        }
+        console.log(this.state.palabra)
+        console.log(this.state.palabraConGuiones)
+    }
+    iniciarJuego(e){
+        if(this.state.palabra.length == '' ){
+            this.socket.emit('getPalabra', 'Servidor echo');
 
+            this.socket.on('Server:palabra', (data) => {
+                this.state.palabra = data;
+                this.state.palabraConGuiones = this.state.palabra.replace(/./g,"_ ")
+                this.palabraGuiones.innerHTML = this.state.palabraConGuiones
+            });
+        }
     }
 
     render(){
         return (
-            <body>
+            <div className="body-game">
                 <h1>Bienvenido al juego del Horcado.</h1>
                 <div  className="card">
                     <div className="carta">
-                        <div className="inputLetra">
-                            <div className="form-floating mb-3">
-                                <input id="letraeEntrada"
-                                       type="text"
-                                       className="form-control"
-                                       placeholder="Letra a buscar"
-                                       maxLength="1"/>
-                                <label htmlFor="floatingInput">Letra a Buscar</label>
+                        <h5>Introduzca una letra para ver si existe.</h5>
+                        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button id="iniciarJuego"
+                                    className="btn btn-outline-success"
+                                    onClick={this.iniciarJuego.bind(this)}>Iniciar</button>
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-4">
+                                <div className="inputLetra">
+                                    <div className="form-floating mb-3">
+                                        <input id="letra"
+                                               name="letra"
+                                               type="text"
+                                               className="form-control"
+                                               placeholder="Letra a buscar"
+                                               value={this.state.label}
+                                               onChange={this.changeField.bind(this)}
+                                               maxLength="1"/>
+                                        <label htmlFor="floatingInput">Letra a Buscar</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-4">
+                                <h2 ref={self => this.palabraGuiones = self}></h2>
                             </div>
                         </div>
                         <div>
                             <button id="IngresarLetra"
-                                    className="btn btn-outline-success"
+                                    className="btn btn-outline-primary"
                                     onClick={this.ingresarletra.bind(this)}>Ingresar Letra</button>
                         </div>
                         <div className="imagenHorcado">
@@ -45,14 +98,10 @@ class App extends React.Component {
                                  src={imagen1}
                                  className="card-img-top"/>
                         </div>
-                        <div>
-                            <p id="palabraGuiones" >
 
-                            </p>
-                        </div>
                     </div>
                 </div>
-            </body>
+            </div>
         )
     }
 }
